d10144e03c9da723bddab392be26b348dca93ee6
leod <subtle.frustration@proton.me>
2017-06-11 17:32:34 +0200

    Add ggez hello world

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+extern crate ggez;
+use ggez::conf;
+use ggez::event;
+use ggez::{GameResult, Context};
+use ggez::graphics;
+use std::time::Duration;
+
+// First we make a structure to contain the game's state
+struct MainState {
+    text: graphics::Text,
+    frames: usize,
+}
+
+// Then we implement the `ggez:event::EventHandler` trait on it, which
+// requires callbacks for updating and drawing the game state each frame.
+//
+// The `EventHandler` trait also contains callbacks for event handling
+// that you can override if you wish, but the defaults are fine.
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 48)?;
+        let text = graphics::Text::new(ctx, "Hello world!", &font)?;
+
+        let s = MainState {
+            text: text,
+            frames: 0,
+        };
+        Ok(s)
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        // Drawables are drawn from their center.
+        let dest_point = graphics::Point::new(self.text.width() as f32 / 2.0 + 10.0,
+                                              self.text.height() as f32 / 2.0 + 10.0);
+        graphics::draw(ctx, &self.text, dest_point, 0.0)?;
+        graphics::present(ctx);
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            println!("FPS: {}", ggez::timer::get_fps(ctx));
+        }
+        Ok(())
+    }
+}
+
+// Now our main function, which does three things:
+//
+// * First, create a new `ggez::conf::Conf`
+// object which contains configuration info on things such
+// as screen resolution and window title,
+// * Second, create a `ggez::game::Game` object which will
+// do the work of creating our MainState and running our game,
+// * then just call `game.run()` which runs the `Game` mainloop.
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("helloworld", "ggez", c).unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
+}
+
