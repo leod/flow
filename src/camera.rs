5bf75d9da8743f106510d2199448951768f2a791
Leonard Dahlmann <leo.dahlmann@gmail.com>
2017-06-15 01:19:39 +0200

    Wasd, fix camera zoom

--- src/camera.rs
+++ src/camera.rs
@@ -3,20 +3,24 @@ use ggez::graphics;
 
 #[derive(Clone, Debug)]
 pub struct Camera {
-   pub position: Vector2<f32>,
-   pub zoom: f32
+    window_width: u32,
+    window_height: u32,
+    pub position: Vector2<f32>,
+    pub zoom: f32
 }
 
 impl Camera {
-    pub fn new() -> Camera {
+    pub fn new(window_width: u32, window_height: u32, zoom: f32) -> Camera {
         Camera {
-            position: Vector2::<f32>::new(0.0, 0.0),
-            zoom: 1.0
+            window_width: window_width,
+            window_height: window_height,
+            position: Vector2::new(0.0, 0.0),
+            zoom: zoom
         }
     }
 
     pub fn transform(self: &Camera, p: Vector2<f32>) -> Vector2<f32> {
-        (p - self.position) * self.zoom
+        (p - self.position) * self.zoom + 0.5 * Vector2::new(self.window_width as f32, self.window_height as f32)
     }
 
     pub fn transform_point(self: &Camera, p: graphics::Point) -> graphics::Point {
