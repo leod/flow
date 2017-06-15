825d103220b1c2eaa8c8f3c72099ce15e8cca96e
leod <subtle.frustration@proton.me>
2017-06-15 02:43:30 +0200

    Use constant for edge length

--- src/hud.rs
+++ src/hud.rs
@@ -7,6 +7,7 @@ use input::{self, Input};
 use camera::Camera;
 use grid::{self, Grid};
 use circuit::Circuit;
+use display;
 
 pub struct Hud {
     mouse_x: i32,
@@ -18,7 +19,7 @@ pub struct Hud {
 
 fn screen_to_grid_coords(camera: &Camera, x: i32, y: i32) -> grid::Coords {
     let mouse_p_t = Vector2::new(x as f32, y as f32);
-    let mouse_p = camera.untransform(mouse_p_t);
+    let mouse_p = camera.untransform(mouse_p_t) / display::EDGE_LENGTH;
     let g_x = mouse_p.x.round() as isize;
     let g_y = mouse_p.y.round() as isize;
 
@@ -77,13 +78,14 @@ impl Hud {
         ctx: &mut Context,
         camera: &Camera
     ) -> GameResult<()> {
-        let grid_coords_t = camera.transform(self.grid_coords.cast());
+        let grid_coords_t = camera.transform(
+            self.grid_coords.cast() * display::EDGE_LENGTH);
 
         let r = graphics::Rect {
             x: grid_coords_t.x,
             y: grid_coords_t.y,
-            w: camera.transform_distance(0.2),
-            h: camera.transform_distance(0.2)
+            w: camera.transform_distance(display::EDGE_LENGTH / 3.0),
+            h: camera.transform_distance(display::EDGE_LENGTH / 3.0)
         };
 
         graphics::set_color(ctx, graphics::Color::new(1.0, 0.0, 0.0, 1.0))?;
