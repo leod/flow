db73305b85bcc8731c65e84eaa3abcd07d15320b
Lars Helge Scheel <larshelge.scheel@gmail.com>
2017-07-09 00:42:49 +0200

    Added Pressure solve, implemented ways to map between matrix indices and node indices

--- src/flow/state.rs
+++ src/flow/state.rs
@@ -6,50 +6,68 @@ use canon_map::CanonMap;
 
 use circuit::{self, Element, Component, Circuit, ComponentId, CellId};
 
-use graph::GraphState;
-
-pub type CellIndex = usize;
-pub type ConnectionIndex = usize;
+use graph::{NodeIndex, GraphState};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Connection {
-    enabled: bool,
-    velocity: f64,
-    resistance: f64,
+    pub enabled: bool,
+    pub velocity: f64,
+    pub resistance: f64,
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct Cell {
     // Whether pressure is to be recomputed
-    bound_pressure: bool,
+    pub bound_pressure: bool,
 
     // Pressure of cell, recomputed every step
-    pressure: f64,
+    pub pressure: f64,
+
+    // for the matrix indices
+    pub mut_idx: Option<usize>,
 }
 
 pub struct State {
-    pub graph: GraphState<CellId, Cell, Connection>
+    pub graph: GraphState<CellId, Cell, Connection>,
+    pub mut_idx_to_node_idx: Vec<NodeIndex>,
 }
 
 impl State {
     pub fn from_circuit(circuit: &Circuit) -> State {
+        let mut node_idx_counter = 0;
+        let mut mut_idx_counter = 0;
+        let mut mut_idx_to_node_idx = Vec::new();
         let graph = GraphState::new(circuit.graph(),
             |(component_id, cell_index), node| {
                 let component =
                     circuit.components().get(&component_id).unwrap();
 
-                match component.element {
+                let res = match component.element {
                     Element::Source =>
                         Cell {
                             bound_pressure: true,
-                            pressure: 100.0
+                            pressure: 100.0,
+                            mut_idx: None,
                         },
-                    _ =>
+                    Element::Sink =>
                         Cell {
+                            bound_pressure: true,
+                            pressure: 0.0,
+                            mut_idx: None,
+                        },
+                    _ => {
+                        let new_cell = Cell {
                             bound_pressure: false,
-                            pressure: 0.0
-                        }
-                }
+                            pressure: 0.0,
+                            mut_idx: Some(mut_idx_counter),
+                        };
+                        mut_idx_to_node_idx.push(node_idx_counter);
+                        mut_idx_counter += 1;
+                        new_cell
+                    },
+                };
+                node_idx_counter += 1;
+                res
             },
             |_, _, _| {
                 Connection {
@@ -60,7 +78,8 @@ impl State {
             });
             
         State {
-            graph: graph
+            graph: graph,
+            mut_idx_to_node_idx: mut_idx_to_node_idx,
         }
     }
 }
