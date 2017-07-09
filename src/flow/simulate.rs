b2b5fd992d6c045794b126e1def4b1882f3bd0e8
Lars Helge Scheel <larshelge.scheel@gmail.com>
2017-07-09 15:20:11 +0200

    Implement accumlation of thoughput

--- src/flow/simulate.rs
+++ src/flow/simulate.rs
@@ -4,7 +4,36 @@ use rulinalg::vector::Vector;
 use flow::state::State;
 
 pub fn time_step(state: &mut State, dt: f64) {
-    // Without advection
+    // Calculate current throughput
+    for node_idx in 0 .. state.graph.num_nodes() {
+        // reset throughput
+        state.graph.node_mut(node_idx).throughput = 0.0;
+        state.graph.node_mut(node_idx).old_throughput = 0.0;
+
+        // step through neigbors -> accumulate positive input
+        // due to divergence = 0 this is working
+        for &(neigh_node_idx, edge_idx) in state.graph.neighbors(node_idx).clone().iter() {
+            let add = {
+                let edge = state.graph.edge(edge_idx);
+                let edge_vel = if node_idx < neigh_node_idx { edge.velocity } else { -edge.velocity };
+                if edge_vel < 0.0 { edge_vel.abs() } else { 0.0 }
+            };
+            state.graph.node_mut(node_idx).old_throughput += add;
+        }
+        println!("{}", state.graph.node(node_idx).old_throughput);
+    }
+
+    for node_idx in 0 .. state.graph.num_nodes() {
+        for &(neigh_node_idx, edge_idx) in state.graph.neighbors(node_idx).clone().iter() {
+            if node_idx < neigh_node_idx {
+                
+            }
+        }
+    }
+
+    // Advection
+    
+
     // solve for pressure
     let num_v = state.mut_idx_to_node_idx.len();
     let mut A = Matrix::<f64>::zeros(num_v, num_v); // system
@@ -31,29 +60,29 @@ pub fn time_step(state: &mut State, dt: f64) {
             // TODO: we have to take care in which direction the flow goes
             let edge = state.graph.edge(edge_idx);
             b[row_id] -= if node_idx < neigh_node_idx { edge.velocity } else { -edge.velocity };
-            println!{"{}", edge.velocity};
+            //println!{"{}", edge.velocity};
             A[[row_id, row_id]] -= 1.0;
         }
     }
 
     // output matrix for debug
-    println!("{:?}", A);
+    //println!("{:?}", A);
 
     // output rhs
-    println!("{:?}", b);
+    //println!("{:?}", b);
 
     // solve this linear system (vll leo sagt 'shit')
     let x = A.solve(b).unwrap();
 
     // output pressures
-    println!("{:?}", x);
+    //println!("{:?}", x);
 
     // write pressures
     for (mut_idx, &node_idx) in state.mut_idx_to_node_idx.iter().enumerate() { 
         state.graph.node_mut(node_idx).pressure = x[mut_idx];
     }
 
-    println!("pressures: {:?}", (0..state.graph.num_nodes()).map(|i| state.graph.node(i).pressure).collect::<Vec<_>>());
+    //println!("pressures: {:?}", (0..state.graph.num_nodes()).map(|i| state.graph.node(i).pressure).collect::<Vec<_>>());
 
     // update velocities for all edges
     for node_idx in 0 .. state.graph.num_nodes() {
@@ -63,9 +92,10 @@ pub fn time_step(state: &mut State, dt: f64) {
                 let press_i = state.graph.node(node_idx).pressure;
                 let press_j = state.graph.node(neigh_node_idx).pressure;
 
-                println!("{}", press_i);
-                println!("{}", press_j);
-                state.graph.edge_mut(edge_idx).velocity += press_j - press_i;
+                let edge = state.graph.edge_mut(edge_idx);
+
+                edge.old_velocity = edge.velocity;
+                edge.velocity += press_j - press_i;
             }
         }
     }
--- src/flow/state.rs
+++ src/flow/state.rs
@@ -12,6 +12,7 @@ use graph::{NodeIndex, GraphState};
 pub struct Connection {
     pub enabled: bool,
     pub velocity: f64,
+    pub old_velocity: f64,
     pub resistance: f64,
 }
 
@@ -22,6 +23,10 @@ pub struct Cell {
 
     // Pressure of cell, recomputed every step
     pub pressure: f64,
+
+    // Throughput
+    pub throughput: f64,
+    pub old_throughput: f64,
 
     // for the matrix indices
     pub mut_idx: Option<usize>,
@@ -47,18 +52,24 @@ impl State {
                         Cell {
                             bound_pressure: true,
                             pressure: 100.0,
+                            throughput: 0.0,
+                            old_throughput: 0.0,
                             mut_idx: None,
                         },
                     Element::Sink =>
                         Cell {
                             bound_pressure: true,
                             pressure: 0.0,
+                            throughput: 0.0,
+                            old_throughput: 0.0,
                             mut_idx: None,
                         },
                     _ => {
                         let new_cell = Cell {
                             bound_pressure: false,
                             pressure: 0.0,
+                            throughput: 0.0,
+                            old_throughput: 0.0,
                             mut_idx: Some(mut_idx_counter),
                         };
                         mut_idx_to_node_idx.push(node_idx_counter);
@@ -73,7 +84,8 @@ impl State {
                 Connection {
                     enabled: true,
                     velocity: 0.0,
-                    resistance: 0.0
+                    old_velocity: 0.0,
+                    resistance: 0.0,
                 }
             });
             
